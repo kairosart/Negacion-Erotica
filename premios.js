8e69fa506b6bb94aaa6d5372df96f0af60367a0d
Kairos Art <kairosart2007@gmail.com>
2025-12-11 19:45:28 +0100

    Final index.html

---
+++ premios.js
@@ -0,0 +1,13 @@
+// premios.js
+const premios = {
+  "🍒🍒🍒":"Arruinado en 1 min.",
+  "🍋🍋🍋":"Arruinado en 3 min.",
+  "🔔🔔🔔":"Arruinado en 5 min.",
+  "💎💎💎":"Arruinado en 10 min.",
+  "⭐⭐⭐":"🎉 ORGASMO!",
+  "🍒🍒":"2' manipulacion dedo clit.",
+  "🍋🍋":"10 azotes en las nalgas",
+  "🔔🔔":"1' de rodillas con el collar y pinzas",
+  "💎💎":"Miembro en vagina con estim. clit.",
+  "⭐⭐":"2' de lengua en el clitoris"
+};
